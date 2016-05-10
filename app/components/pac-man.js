cb08b813d3889135a03d48a07f894efffb702b9d
Kori Roys <kori@koriroys.com>
2016-05-11 00:29:22 +0200

    smooth movement

--- app/components/pac-man.js
+++ app/components/pac-man.js
@@ -16,6 +16,17 @@ export default Ember.Component.extend(KeyboardShortcuts, {
     [2, 2, 2, 2, 2, 2, 2, 1],
     [1, 2, 2, 2, 2, 2, 2, 1],
   ],
+  isMoving: false,
+  direction: 'stopped',
+
+
+  directions: {
+   'up': {x: 0, y: -1},
+   'down': {x: 0, y: 1},
+   'left': {x: -1, y: 0},
+   'right': {x: 1, y: 0},
+   'stopped': {x: 0, y: 0}
+  },
 
   screenWidth: Ember.computed(function() {
     return this.get('grid.firstObject.length');
@@ -76,7 +87,7 @@ export default Ember.Component.extend(KeyboardShortcuts, {
 
   drawPellet(x, y) {
     let radiusDivisor = 6;
-    this.drawCircle(x, y, radiusDivisor);
+    this.drawCircle(x, y, radiusDivisor, 'stopped');
   },
 
   drawWall(x, y) {
@@ -95,21 +106,26 @@ export default Ember.Component.extend(KeyboardShortcuts, {
     let x = this.get('x');
     let y = this.get('y');
     let radiusDivisor = 2;
-    this.drawCircle(x, y, radiusDivisor);
+    this.drawCircle(x, y, radiusDivisor, this.get('direction'));
   },
 
-  drawCircle: function(x, y, radiusDivisor) {
+  drawCircle: function(x, y, radiusDivisor, direction) {
     let ctx = this.get('ctx');
     let squareSize = this.get('squareSize');
 
-    let pixelX = (x + 1/2) * squareSize;
-    let pixelY = (y + 1/2) * squareSize;
+    let pixelX = (x + 1/2 + this.offsetFor('x', direction)) * squareSize;
+    let pixelY = (y + 1/2 + this.offsetFor('y', direction)) * squareSize;
 
     ctx.fillStyle = '#000';
     ctx.beginPath();
     ctx.arc(pixelX, pixelY, squareSize / radiusDivisor, 0, Math.PI * 2, false);
     ctx.closePath();
     ctx.fill();
+  },
+
+  offsetFor(coordinate, direction) {
+    let frameRatio = this.get('frameCycle') / this.get('framesPerMovement');
+    return this.get(`directions.${direction}.${coordinate}`) * frameRatio;
   },
 
   clearScreen() {
@@ -120,15 +136,37 @@ export default Ember.Component.extend(KeyboardShortcuts, {
     ctx.clearRect(0, 0, screenPixelWidth, screenPixelHeight);
   },
 
-  movePacMan(direction, amount) {
-    this.incrementProperty(direction, amount);
-
-    if (this.collideWithBorder() || this.collideWithWall()) {
-      this.decrementProperty(direction, amount);
+  movePacMan(direction) {
+    if (this.get('isMoving') || this.pathBlockedInDirection(direction)) {
+      // let animation finish
+    } else {
+      this.set('direction', direction);
+      this.set('isMoving', true);
+      this.movementLoop();
     }
 
-    this.processAnyPellets();
+    this.clearScreen();
+    this.drawGrid();
+    this.drawPac();
+  },
 
+  frameCycle: 1,
+  framesPerMovement: 30,
+
+  movementLoop() {
+    if (this.get('frameCycle') == this.get('framesPerMovement')) {
+      let direction = this.get('direction');
+      this.set('x', this.nextCoordinate('x', direction));
+      this.set('y', this.nextCoordinate('y', direction));
+
+      this.set('isMoving', false);
+      this.set('frameCycle', 1);
+
+      this.processAnyPellets();
+    } else {
+      this.incrementProperty('frameCycle');
+      Ember.run.later(this, this.movementLoop, 1000/60);
+    }
     this.clearScreen();
     this.drawGrid();
     this.drawPac();
@@ -164,38 +202,26 @@ export default Ember.Component.extend(KeyboardShortcuts, {
     });
   },
 
-  collideWithWall() {
-    let x = this.get('x');
-    let y = this.get('y');
-    let grid = this.get('grid');
-
-    return grid[y][x] === 1;
+  nextCoordinate(coordinate, direction) {
+    return this.get(coordinate) + this.get(`directions.${direction}.${coordinate}`);
   },
 
-  collideWithBorder() {
-    let x = this.get('x');
-    let y = this.get('y');
-    let screenHeight = this.get('screenHeight');
-    let screenWidth = this.get('screenWidth');
+  pathBlockedInDirection(direction) {
+    let cellTypeInDirection = this.cellTypeInDirection(direction);
+    return Ember.isEmpty(cellTypeInDirection) || cellTypeInDirection === 1;
+  },
 
-    return x < 0 ||
-      y < 0 ||
-      x >= screenWidth ||
-      y >= screenHeight;
+  cellTypeInDirection(direction) {
+    let nextX = this.nextCoordinate('x', direction);
+    let nextY = this.nextCoordinate('y', direction);
+
+    return this.get(`grid.${nextY}.${nextX}`);
   },
 
   keyboardShortcuts: {
-    up() {
-      this.movePacMan('y', -1);
-    },
-    down() {
-      this.movePacMan('y', 1);
-    },
-    left() {
-      this.movePacMan('x', -1);
-    },
-    right() {
-      this.movePacMan('x', 1);
-    }
+    up() { this.movePacMan('up'); },
+    down() { this.movePacMan('down'); },
+    left() { this.movePacMan('left'); },
+    right() { this.movePacMan('right'); }
   }
 });
